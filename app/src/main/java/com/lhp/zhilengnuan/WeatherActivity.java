25136869f2550cc3bee1a25dbca487a99f883464
刘海朋 <991695066@qq.com>
2017-11-14 12:55:24 +0800

    后台自动更新天气

--- app/src/main/java/com/lhp/zhilengnuan/WeatherActivity.java
+++ app/src/main/java/com/lhp/zhilengnuan/WeatherActivity.java
@@ -1,6 +1,7 @@
 package com.lhp.zhilengnuan;
 
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -23,6 +24,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.lhp.zhilengnuan.gson.Forecast;
 import com.lhp.zhilengnuan.gson.Weather;
+import com.lhp.zhilengnuan.service.AutoUpdateService;
 import com.lhp.zhilengnuan.utils.HttpUtil;
 import com.lhp.zhilengnuan.utils.Utility;
 
@@ -188,6 +190,8 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+
+        startService(new Intent(this, AutoUpdateService.class));
     }
 
     private void initView() {
---
+++ app/src/main/java/com/lhp/zhilengnuan/service/AutoUpdateService.java
@@ -0,0 +1,96 @@
+package com.lhp.zhilengnuan.service;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.IBinder;
+import android.os.SystemClock;
+import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
+
+import com.lhp.zhilengnuan.Config;
+import com.lhp.zhilengnuan.gson.Weather;
+import com.lhp.zhilengnuan.utils.HttpUtil;
+import com.lhp.zhilengnuan.utils.Utility;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
+/**
+ * Created by lhp on 2017/11/14.
+ * description: 后台自动更新天气
+ */
+
+public class AutoUpdateService extends Service {
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        updateWeather();
+        updateBingPic();
+        AlarmManager manager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        int anHour = 8 * 60 * 60 * 1000;//8小时毫秒数
+        long triggerAtTime = SystemClock.elapsedRealtime() + anHour;
+        Intent i = new Intent(this, AutoUpdateService.class);
+        PendingIntent pi = PendingIntent.getActivity(this, 0, i, 0);
+        manager.cancel(pi);
+        manager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, triggerAtTime, pi);
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    private void updateBingPic() {
+        HttpUtil.sendOkHttpRequest(Config.BING_PIC, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String bingPicStr = response.body().string();
+                SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(AutoUpdateService.this).edit();
+                editor.putString("bing_pic", bingPicStr);
+                editor.apply();
+            }
+        });
+    }
+
+    private void updateWeather() {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String weatherString = preferences.getString("weather", null);
+        if (weatherString != null) {
+            Weather weather = Utility.handleWeatherResponse(weatherString);
+            String weather_id = weather.basic.weatherId;
+
+            String weatherUrl = Config.API_WEATHER + weather_id + "&key=" + Config.WEATHER_KEY;
+            HttpUtil.sendOkHttpRequest(weatherUrl, new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    final String responseText = response.body().string();
+                    final Weather weather = Utility.handleWeatherResponse(responseText);
+
+                    if (weather != null && "ok".equals(weather.status)) {
+                        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(AutoUpdateService.this).edit();
+                        editor.putString("weather", responseText);
+                        editor.apply();
+
+                    }
+                }
+            });
+        }
+    }
+}
